03b97d0fd0865df07a63258cd629d1ebb63e61e3
tuliomoreira77 <39540679+tuliomoreira77@users.noreply.github.com>
2021-05-13 13:36:30 -0300

    backend

---
+++ gerador-curriculos-backend/dist/app.js
@@ -0,0 +1,81 @@
+"use strict";
+var __awaiter = (this && this.__awaiter) || function (thisArg, _arguments, P, generator) {
+    function adopt(value) { return value instanceof P ? value : new P(function (resolve) { resolve(value); }); }
+    return new (P || (P = Promise))(function (resolve, reject) {
+        function fulfilled(value) { try { step(generator.next(value)); } catch (e) { reject(e); } }
+        function rejected(value) { try { step(generator["throw"](value)); } catch (e) { reject(e); } }
+        function step(result) { result.done ? resolve(result.value) : adopt(result.value).then(fulfilled, rejected); }
+        step((generator = generator.apply(thisArg, _arguments || [])).next());
+    });
+};
+Object.defineProperty(exports, "__esModule", { value: true });
++;
+const express = require("express");
+const pdf = require("html-pdf");
+const cors = require("cors");
+const pupeeteer = require("puppeteer");
+const app_port = 5000;
+const app = express();
+app.use(express.json({ limit: '10mb' }));
+app.use(cors());
+let chromium;
+const pdfConfig = {
+    format: 'A4',
+    orientation: 'portrait',
+};
+app.listen(app_port, () => __awaiter(void 0, void 0, void 0, function* () {
+    console.log(`Servidor iniciado na porta ${app_port}`);
+    chromium = yield launchChromium();
+}));
+app.post('/v1/generate/pdf', (req, res) => __awaiter(void 0, void 0, void 0, function* () {
+    try {
+        let htmlTemplate = req.body.html;
+        let generatedPdf = yield generatePdfChromium(htmlTemplate);
+        res.send({ pdf: generatedPdf.toString('base64') });
+    }
+    catch (err) {
+        console.log(err);
+        res.status(500).send('Erro Interno Servidor');
+    }
+}));
+function generatePdf(html) {
+    return __awaiter(this, void 0, void 0, function* () {
+        return new Promise((resolve, reject) => {
+            pdf.create(html, pdfConfig).toBuffer((err, buffer) => {
+                if (err) {
+                    reject(err);
+                }
+                resolve(buffer);
+            });
+        });
+    });
+}
+function launchChromium() {
+    return __awaiter(this, void 0, void 0, function* () {
+        const chromium = yield pupeeteer.launch({
+            headless: true,
+            args: [
+                '--no-sandbox'
+            ]
+        });
+        return chromium;
+    });
+}
+function generatePdfChromium(html) {
+    return __awaiter(this, void 0, void 0, function* () {
+        const page = yield chromium.newPage();
+        yield page.setContent(html, { waitUntil: 'networkidle2' });
+        yield sleep(2000);
+        const pdfBuffer = yield page.pdf({ format: 'a4', pageRanges: '1', printBackground: true });
+        yield page.close();
+        return pdfBuffer;
+    });
+}
+function sleep(time) {
+    return __awaiter(this, void 0, void 0, function* () {
+        return new Promise((resolve, reject) => {
+            setTimeout(() => { resolve(true); }, time);
+        });
+    });
+}
+//# sourceMappingURL=app.js.map
---
+++ gerador-curriculos-backend/src/app.ts
@@ -0,0 +1,67 @@
++import * as express from 'express';
+import * as pdf from 'html-pdf';
+import * as cors from 'cors';
+import * as pupeeteer from 'puppeteer';
+
+const app_port = 5000;
+const app = express();
+app.use(express.json({limit: '10mb'}));
+app.use(cors());
+let chromium:pupeeteer.Browser;
+
+const pdfConfig:pdf.CreateOptions = {
+    format: 'A4',
+    orientation: 'portrait',
+}
+
+app.listen(app_port, async () => {
+    console.log(`Servidor iniciado na porta ${app_port}`);
+    chromium = await launchChromium();
+});
+
+app.post('/v1/generate/pdf', async(req, res) => {
+    try {
+        let htmlTemplate = req.body.html;
+        let generatedPdf = await generatePdfChromium(htmlTemplate);
+        res.send({pdf: generatedPdf.toString('base64')});
+    } catch(err) {
+        console.log(err);
+        res.status(500).send('Erro Interno Servidor');
+    }
+});
+
+async function generatePdf(html:string):Promise<Buffer> {
+    return new Promise((resolve, reject) => {
+        pdf.create(html, pdfConfig).toBuffer((err, buffer) => {
+            if(err) {
+                reject(err);
+            }
+            resolve(buffer);
+        });
+    });
+}
+
+async function launchChromium() {
+    const chromium = await pupeeteer.launch({
+        headless: true,
+        args: [
+            '--no-sandbox'
+        ]
+    });
+    return chromium;
+}
+
+async function generatePdfChromium(html:string) {
+    const page = await chromium.newPage();
+    await page.setContent(html, {waitUntil: 'networkidle2'});
+    await sleep(2000);
+    const pdfBuffer = await page.pdf({format: 'a4', pageRanges: '1', printBackground: true});
+    await page.close();
+    return pdfBuffer; 
+}
+
+async function sleep(time:number) {
+    return new Promise((resolve, reject) => {
+        setTimeout(() => {resolve(true)}, time)
+    });
+}
